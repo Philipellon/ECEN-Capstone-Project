83125b58364e666942deac938e54b0fd10f1b9cb
Philipellon <philipellon@tamu.edu>
2025-10-21 14:53:06 -0500

    Update prototype4.h

--- prototype4.h
+++ prototype4.h
@@ -39,19 +39,19 @@ extern "C" {
 #define TRANS8  GPIO_NUM_1
 
 // ---------- Ultrasonic pins ----------
-#define ECHO1     GPIO_NUM_7
-#define ECHO2     GPIO_NUM_38
-#define ECHO3     GPIO_NUM_17
-#define ECHO4     GPIO_NUM_18
-#define ECHO5     GPIO_NUM_8
-#define ECHO6     GPIO_NUM_35
+#define ECHO1     GPIO_NUM_39
+#define ECHO2     GPIO_NUM_7
+#define ECHO3     GPIO_NUM_9
+#define ECHO4     GPIO_NUM_11
+#define ECHO5     GPIO_NUM_16
+#define ECHO6     GPIO_NUM_18
 
-#define TRIGGER1  GPIO_NUM_16
-#define TRIGGER2  GPIO_NUM_15
-#define TRIGGER3  GPIO_NUM_9
+#define TRIGGER1  GPIO_NUM_38
+#define TRIGGER2  GPIO_NUM_40
+#define TRIGGER3  GPIO_NUM_8
 #define TRIGGER4  GPIO_NUM_10
-#define TRIGGER5  GPIO_NUM_11
-#define TRIGGER6  GPIO_NUM_36
+#define TRIGGER5  GPIO_NUM_15
+#define TRIGGER6  GPIO_NUM_17
 
 // ---------- Joystick ADC channels ----------
 #define JOYSTICK_UD  ADC1_CHANNEL_5
@@ -61,20 +61,13 @@ extern "C" {
 #define ADC_WIDTH    ADC_WIDTH_BIT_12
 #define ADC_ATTEN    ADC_ATTEN_DB_12
 
-// Joystick calibration
-extern int UPMID;
-extern int LRMID;
-extern int sensor_fault;
-
 // Prototypes
 void  echo_sensor(void *pvParameter);
 void  motor_controller(void);
 void  init_gpio(void);
-void  init_adc(void);
-int   read_joystick(adc_channel_t channel);
-int   calibrate_joystick(adc_channel_t channel);
 float trigger_sensor(gpio_num_t trigger_pin, gpio_num_t echo_pin);
 
 #ifdef __cplusplus
 }
 #endif
+
